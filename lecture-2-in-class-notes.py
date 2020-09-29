31d3fdef2961ecb590328f70316195458c3ed8d9
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-09-30 00:42:09 +0300

    Add notes for the Lecture-2

---
+++ lecture-2-in-class-notes.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 21:04:36 2020
+
+@author: YCP
+"""
+
+# hi = "Hello there"
+# name = "Mehmet"
+
+# greeting = hi + " " + name
+
+# print(greeting)
+
+# silly = greeting*3
+# print(silly, name)
+
+# x = 1 
+
+# x_1 = str(x)
+
+# print("My favorite num is", x, ".")
+# print("My favorite num is" + " " + x_1 + ".")
+
+# n = 0
+# while n <= 5: 
+#     print("n is less than 5. n:", n)
+#     n = n + 1
+
+# for n in range(5): 
+#     print(n)
+    
