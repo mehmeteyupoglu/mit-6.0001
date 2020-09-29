bb1d149df3ac75b2f0e24b2baded99c6d65cb0a3
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-09-29 20:57:35 +0300

    Solve pset0

---
+++ ps0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 20:44:17 2020
+
+@author: Mehmet Eyüpoğlu
+
+"""
+
+import math 
+x = int(input("Enter a number (x): "))
+y = int(input("Enter another number (y): "))
+
+print( "The result of x**y: ", x**y)
+
+print("Logarithm of ", x, " is ", math.log(x, 2))
