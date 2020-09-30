4c4dffc0a1ec23b8e5708b43bd77d263aeaec372
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-09-30 13:41:04 +0300

    Update lecture-2 notes

--- lecture-3-in-class-notes.py
+++ lecture-3-in-class-notes.py
@@ -5,18 +5,6 @@ Created on Wed Sep 30 09:41:28 2020
 @author: Mehmet Eyüpoğlu
 """
 
-# cube = 8
-
-# for guess in range(abs(cube + 1)):
-#     if guess**3 >= abs(cube): 
-#         break
-# if guess**3 != abs(cube): 
-#     print(cube, "is not a perfect cube")
-# else: 
-#     if cube < 0: 
-#         guess = -guess
-#     print("Cube root of '+str(cube)+' is '+str(guess)' " )
-
 # print(len("Hello World!"))
 # print("Hello World!"[0])
 # print("Hello World!"[-2])
@@ -35,8 +23,8 @@ Created on Wed Sep 30 09:41:28 2020
 #     print(space*i, name[i])
 
 #######GUESS-AND-CHECK-CUBE-ROOT-EXERCISE#######
-cube = 16
+# cube = 8
 
-for guess in range(cube+1): 
-    if guess**3 == cube: 
-        print("Cube root of", cube, "is", guess)
+# for guess in range(cube+1): 
+#     if guess**3 == cube: 
+#         print("Cube root of", cube, "is", guess)
