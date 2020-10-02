c73f431ffaf26f5281a4625dca39fe53705f88ee
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-10-03 00:25:11 +0300

    Update lecture-5

---
+++ lecture-5a-in-class-notes.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 23:37:15 2020
+
+@author: YCP
+"""
+t = [1,2,3,4,5]
+# total = 0
+
+# for i in t: 
+#     total += i
+    
+
+# print(total)
+
+# del(t[0])
+
+# print(t)
+
+# t = [1] + t
+# print(t)
+
+s = "12,;'';dsfa, ' ,,,d;"
+
+l = s.split(",")
+print(l)
+
+l_2 = "_".join(l)
+print(l_2)
+
+    
